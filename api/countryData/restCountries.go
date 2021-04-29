2b7b7f9645be17d334af683a2808676492f8ecc6
Sully <susanse@stud.ntnu.no>
2021-04-29 14:12:36 +0200

    added comments, edited readme

--- api/countryData/restCountries.go
+++ api/countryData/restCountries.go
@@ -10,23 +10,25 @@ import (
 	"strings"
 )
 
-//Struct containing all information from restcountries
+//Information -Struct containing all necessary information from restcountries
 type Information []struct {
 	Name       string `json:"name"`
 	Alpha2Code string `json:"alpha2Code"`
 	Capital    string `json:"capital"`
 }
 
-//Struct containing error message
+//MyError -Struct containing custom error message
 type MyError struct {
 	What string
 }
 
-// Class function will be called and handle all requests and fetches
+//Handler - Class function will be called and handle all requests and fetches
 func (data *Information) Handler(country string) (int, error) {
-	//Check if document exists
-	if _, err := os.Stat("./data/countries.json"); os.IsNotExist(err) { //if it does not exist:
-		//Fetch information
+
+	//Check if document exists, if it does not exist run this code:
+	if _, err := os.Stat("./data/countries.json"); os.IsNotExist(err) {
+
+		//Fetch information:
 		var input Information
 		status, err := input.req("https://restcountries.eu/rest/v2/all")
 		if err != nil {
@@ -35,11 +37,10 @@ func (data *Information) Handler(country string) (int, error) {
 
 		//Store it in a file:
 		file, _ := json.MarshalIndent(input, "", " ")
-
 		_ = ioutil.WriteFile("./data/countries.json", file, 0644)
 	}
 
-	//If you want a specific country
+	//If function was called with a specific country
 	if country != "" {
 		var specificCountry Information
 		status, err := specificCountry.oneCountry(country)
@@ -49,7 +50,7 @@ func (data *Information) Handler(country string) (int, error) {
 		}
 		return http.StatusOK, nil
 
-	} else { //if you dont require a specific country:
+	} else { //If function was NOT called with a specific country:
 		var AllData Information
 		status, err := AllData.allCountries()
 		if err != nil {
@@ -60,7 +61,7 @@ func (data *Information) Handler(country string) (int, error) {
 	}
 }
 
-//req requests information
+//req -Requests information from the api
 func (data *Information) req(url string) (int, error) {
 	output, status, jsonErr := api.RequestData(url)
 
@@ -75,7 +76,7 @@ func (data *Information) req(url string) (int, error) {
 	return http.StatusOK, jsonErr
 }
 
-//oneCountry gets information about one specific country
+//oneCountry -Gets information about one specific country from local storage
 func (data *Information) oneCountry(countryName string) (int, error) {
 	countryName = strings.Title(strings.ToLower(countryName))
 	//Get data from document
@@ -89,21 +90,19 @@ func (data *Information) oneCountry(countryName string) (int, error) {
 		return http.StatusInternalServerError, err
 	}
 
+	//Looping to look for the specific country
 	for _, v := range *data {
 		if v.Name == countryName {
 
-			//only return one data instance
+			//When found, break the loop and return that one instance
 			*data = Information{{v.Name, v.Alpha2Code, v.Capital}}
-
 			return http.StatusOK, nil
 		}
-
 	}
-
 	return http.StatusBadRequest, &MyError{"Country:" + countryName + " was not found in the database"}
 }
 
-//allCountries gets information about all countries
+//allCountries -Gets information about all countries from local storage
 func (data *Information) allCountries() (int, error) {
 	//Get data from document
 	file, err := ioutil.ReadFile("./data/countries.json")
@@ -119,7 +118,7 @@ func (data *Information) allCountries() (int, error) {
 	return http.StatusOK, nil
 }
 
-//From restStub (https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2021/-/tree/master/RESTstub)
+//ParseFile -From restStub (https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2021/-/tree/master/RESTstub)
 func ParseFile(filename string) ([]byte, int, error) {
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -128,7 +127,7 @@ func ParseFile(filename string) ([]byte, int, error) {
 	return file, http.StatusOK, err
 }
 
-//Custom error function
+//Error -Custom error function
 func (e *MyError) Error() string {
 	return fmt.Sprintf("Error: %s",
 		e.What)
