704135cbf5a78fd06a1e38c1e09c2585f6c3f2e7
RLoland <rickarl@stud.ntnu.no>
2021-05-10 12:22:13 +0200

    Initial weatherhook work

---
+++ api/weatherHook/MethodHandler.go
@@ -0,0 +1,27 @@
+package weatherHook
+
+import (
+	"main/debug"
+	"net/http"
+)
+
+// MethodHandler - handles the method of a http request.
+func MethodHandler(w http.ResponseWriter, r *http.Request) {
+	var weatherHook WeatherHook
+	switch r.Method {
+	case http.MethodPost:
+		weatherHook.HandlerPost(w,r)
+	case http.MethodGet:
+		weatherHook.HandlerGet(w,r)
+	case http.MethodDelete:
+		weatherHook.HandlerDelete(w,r)
+	default:
+		debug.ErrorMessage.Update(
+			http.StatusMethodNotAllowed,
+			"MethodHandler() -> Validating method",
+			"method validation: wrong method",
+			"Method not implemented.",
+		)
+		debug.ErrorMessage.Print(w)
+	}
+}
